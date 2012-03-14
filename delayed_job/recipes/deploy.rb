1e8b79e70f067d268d4e10cb8073ba0047483654
David Rice <me@davidjrice.co.uk>
2012-03-14 19:54:21 +0000

    Handle env vars

--- delayed_job/recipes/deploy.rb
+++ delayed_job/recipes/deploy.rb
@@ -5,4 +5,8 @@ node[:deploy].each do |application, deploy|
     command "cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} bundle install"
   end
 
+  execute "add env vars" do
+    command "ln -s #{deploy[:deploy_to]}/shared/.env #{deploy[:deploy_to]}/current/.env"
+  end
+
 end
