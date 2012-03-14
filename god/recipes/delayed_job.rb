e030052a1a99d309d8cd67dafea8724b349cb8ec
David Rice <me@davidjrice.co.uk>
2012-03-14 19:29:37 +0000

    Deploy

--- god/recipes/delayed_job.rb
+++ god/recipes/delayed_job.rb
@@ -1,12 +1,15 @@
-template "/etc/god/conf.d/delayed_job.god" do
-  source "delayed_job.god.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  variables(
-    :deploy_to => deploy[:deploy_to],
-    :rails_env => deploy[:rails_env],
-    :queue => node[:delayed_job][:queue]
-  )
-  notifies :restart, resources(:service => "god")
+node[:deploy].each do |application, deploy|
+  next unless node[:delayed_job][:applications].include?(application)
+  template "/etc/god/conf.d/delayed_job.god" do
+    source "delayed_job.god.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+      :deploy_to => deploy[:deploy_to],
+      :rails_env => deploy[:rails_env],
+      :queue => node[:delayed_job][:queue]
+    )
+    notifies :restart, resources(:service => "god")
+  end
 end
