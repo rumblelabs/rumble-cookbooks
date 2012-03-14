d4552ad319f475a438eefbac8d14a2a872c6c53b
David Rice <me@davidjrice.co.uk>
2012-03-14 00:50:57 +0000

    Rvm

--- jenkins/recipes/default.rb
+++ jenkins/recipes/default.rb
@@ -1,16 +1,8 @@
-# include_recipe "rvm::default"
 bash "installing rvm for jenkins" do
   user "jenkins"
   code "bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)"
   not_if "test -e /var/lib/jenkins/.rvm"
 end
-
-# remote_file "/var/lib/jenkins/rvm-installer" do
-#   source "https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer"
-#   owner "jenkins"
-#   group "jenkins"
-#   mode "0755"
-# end
 
 install_starts_service = true
 include_recipe "jenkins::jenkins"
