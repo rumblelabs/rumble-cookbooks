9a35a3005d66773d5b89d891da237b5dff55df1c
David Rice <me@davidjrice.co.uk>
2012-03-14 02:32:52 +0000

    jenkins git config

--- jenkins/recipes/plugins.rb
+++ jenkins/recipes/plugins.rb
@@ -11,3 +11,14 @@ template "#{node[:jenkins][:server][:home]}/jenkins.plugins.hipchat.HipChatNotif
     :hipchat_room     => node[:jenkins][:hipchat][:room]
   )
 end
+
+template "#{node[:jenkins][:server][:home]}/hudson.plugins.git.GitSCM.xml" do
+  source      "jenkins.git.config.erb"
+  owner       'jenkins'
+  group       'jenkins'
+  mode        '0644'
+  variables(
+    :git_name        => node[:jenkins][:git][:name],
+    :git_email       => node[:jenkins][:git][:email]
+  )
+end
