718f959a8bd151afc4c9ce643082d758f6b6911c
David Rice <me@davidjrice.co.uk>
2013-02-10 22:17:37 +0000

    Start god earlier

--- delayed_job/recipes/deploy.rb
+++ delayed_job/recipes/deploy.rb
@@ -1,19 +1,19 @@
 node[:deploy].each do |application, deploy|
   next unless node[:delayed_job][:applications].include?(application)
 
+  execute "start god" do
+    command "sudo god -c /etc/god/master.god"
+    not_if do
+      `ps xU root | grep god` =~ /ruby/
+    end
+  end
+
   execute "update app" do
     command "cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} bundle install"
   end
 
   execute "add env vars" do
     command "ln -s #{deploy[:deploy_to]}/shared/.env #{deploy[:deploy_to]}/current/.env"
-  end
-
-  execute "start god" do
-    command "sudo god -c /etc/god/master.god"
-    not_if do
-      `ps xU root | grep god` =~ /ruby/
-    end
   end
 
   execute "restart delayed job" do
